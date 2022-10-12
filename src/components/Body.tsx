e9abd76bf0d01804a32fe6297c6862b5859616fd
Vojtěch Döme <ojta@post.cz>
2022-10-12 09:14:09 +0200

    Images

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,5 @@
-import animatePic from "../images/animate.png";
-import AI from "../images/AI.png";
+import animatePic from "../images/hero.png";
+import AI from "../images/left_body.png";
 import Heading from "./Heading";
 import { FourBox, TwoBox, TwoByTwo } from "./Box";
 import Hero from "./Hero";
@@ -10,13 +10,13 @@ function Body() {
       <div className="flex flex-col items-center ml-24 mr-24">
         <div className="flex flex-col m-12 items-center w-full">
           <Hero />
-          <img src={animatePic} alt="animate" className="m-10 rounded-xl w-full" />
+          <img src={animatePic} id="border-green" alt="animate" className="m-10 rounded-r-xl" />
           <Heading upperTitle="Whats the function" title="Meet the feature of products" />
         </div>
         <FourBox />
         <div className="flex flex-row m-12 items-center w-full">
           <div className="flex w-1/2 mr-8">
-            <img src={AI} alt="animate" className="rounded-xl w-full" />
+            <img src={AI} id="border-green" alt="animate" className="rounded-xl w-full" />
           </div>
           <div className="flex flex-col ml-8 w-1/2">
             <Heading onLeft={true} upperTitle="Our services" title="Bussiness Goals Achived with Design" />
